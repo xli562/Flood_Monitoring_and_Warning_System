396d9732eccaa164eca5cfc87aea0e63233598b6
xli562 <116271608+xli562@users.noreply.github.com>
2023-02-26 20:45:49 +0000

    Finished prediction part using excel

--- Task2G.py
+++ Task2G.py
@@ -1,39 +1,94 @@
+from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
-import pmdarima as pm
-from pmdarima import model_selection
+def mapping(inlst: list, lout: int, hout: int) -> list | int:
+    """ Takes inlst [the INput LiST] and compresses it to have length as close to lout [Length of OUTput] as possible, 
+    and times all elements by hout [Hight of OUTput.]"""
+    outlst = [] # the list to be outputted
+    length = len(inlst)
+    if lout == 0:
+        lout = length
+    step = int(length / lout)
+    for i in range(lout):
+        outlst.append(inlst[i]*hout)
+    return outlst
+
+from openpyxl import Workbook, load_workbook
+
 import numpy as np
+import pandas as pd
 import datetime
-from datetime import timedelta
 from matplotlib import pyplot as plt
 
-def data(stations: list, days: timedelta) -> list:
-    """Takes a list of MonitoringStation objects
-    and the time range interested in the datetime.timedelta form.
-    
-    Outputs a list of 3-element tuples:
-    (Name of station: str, 
-    historic relative water level readings: list, 
-    the days on which the recordings were made: list)"""
+# customisable inputs
+stationName = 'Cam'
+scaleFactor = 100000
+futureDays = 1
 
-def sarima_predict()
+future15Mins = 96 * futureDays  # 96 15-minutes in a day
 
-measure_id = 'http://environment.data.gov.uk/flood-monitoring/id/measures/2514-level-stage-i-15_min-mASD'
-dt = datetime.timedelta(365)
-data = np.asarray(fetch_measure_levels(measure_id, dt)[1])
+# get raw data
+stations = build_station_list()
+for i in stations: 
+    if i.name == stationName:
+        measure_id = i.measure_id
+        station = i
+dt = datetime.timedelta(1)
+histLvAndTime = fetch_measure_levels(measure_id, dt)
+levelsLst = histLvAndTime[1] # absolute levels list
+
+#data conditioning
+rlevLst = []# relative level list
+trange = station.typical_range
+for i in levelsLst:
+    if trange != None and i != None:
+        rlevel = (i - trange[0]) / (trange[1] - trange[0])  # relative level
+        rlevLst.append(rlevel)
+
+data = mapping(rlevLst, 0, scaleFactor)
+datLeng = len(data)
+timeAxis = []
+for i in histLvAndTime[0]:
+    timeAxis.append(i.strftime("%Y/%m/%d %H:%M"))
+last_data_time = histLvAndTime[0][-1]
+time = last_data_time
+first_data_time = histLvAndTime[0][0].strftime("%Y/%m/%d %H:%M")
+for i in range(future15Mins):
+    time += datetime.timedelta(0,0,0,0,15)
+    timeAxis.append(time.strftime("%Y/%m/%d %H:%M"))
 
 
-train, test = model_selection.train_test_split(data, train_size=0.7)
+# init excel
+filename = "data_try.xlsx"
 
-arima_model = pm.auto_arima(train, error_action='ignore', trace=True,
-                      suppress_warnings=True, maxiter=10,
-                      seasonal=True, m=20)
-#automatically fit the optimal ARIMA model for given time series
-arima_model_fitted = pm.auto_arima(data)
-# one-step out-of sample forecast
-forecast = arima_model.predict(n_periods=100)
+# Writing dates with pandas
+timeAxis = pd.DataFrame({'Sample Times':pd.date_range(first_data_time, periods=datLeng+future15Mins, freq=datetime.timedelta(0,0,0,0,15)) })
+writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+timeAxis.to_excel(writer, sheet_name='test', index=False)
+workbook  = writer.book
+worksheet = writer.sheets['test']
+formatdict = {'num_format':'yyyy/mm/dd hh/mm'}
+fmt = workbook.add_format(formatdict)
+worksheet.set_column('A:A', None, fmt)
+writer.save()
+writer.close()
 
-x = np.arange(test.shape[0])
-plt.plot(x, test)
-plt.plot(np.arange(forecast.shape[0]), forecast)
-plt.title('Actual test samples vs. forecasts')
-plt.show()
+# Writing water level with openpyxl
+workbook = load_workbook(filename=filename)
+sheet = workbook.active
+for i in range(1, datLeng):
+    sheet.cell(row=i+1, column=2).value = data[-i]
+sheet.cell(row=datLeng+1, column=2).value = data[0]
+
+# forecasting with openpyxl
+for i in range(2, future15Mins+2):
+    # predicted value
+    sheet.cell(row = datLeng+i, column = 3).value = f'=FORECAST.ETS(A{datLeng+1},$B$1:$B${datLeng},$A$1:$A${datLeng},0,1)'
+    # upper bound
+    sheet.cell(row = datLeng+i, column = 4).value = f'=FORECAST.ETS(A{datLeng+1},$B$1:$B${datLeng},$A$1:$A${datLeng},0,1)+FORECAST.ETS.CONFINT(A{datLeng+1},$B$1:$B${datLeng},$A$1:$A${datLeng},0.97,0,1)'
+    # lower bound
+    sheet.cell(row = datLeng+i, column = 5).value = f'=FORECAST.ETS(A{datLeng+1},$B$1:$B${datLeng},$A$1:$A${datLeng},0,1)-FORECAST.ETS.CONFINT(A{datLeng+1},$B$1:$B${datLeng},$A$1:$A${datLeng},0.97,0,1)'
+
+workbook.save(filename=filename)
+
+# Reading from the excel file
+
