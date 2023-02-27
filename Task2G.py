cfcb233658e666747142e0eb037dc33d191093fe
Jc2404 <jc2404@cam.ac.uk>
2023-02-27 22:39:10 +0000

    Testing

--- Task2G.py
+++ Task2G.py
@@ -1,5 +1,38 @@
 from floodsystem.oracle import predict
 from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 
-a = predict(build_station_list(), pastDays=2, futureDays = 1, stationsCount=2, usePrevExcel=False, seasonal=True)
-print(a)
+a = predict(build_station_list(), pastDays=2, futureDays = 1, stationsCount=3, usePrevExcel=True, seasonal=True)
+
+testation = list(a.keys())
+testlevel = list(a.values())
+
+stations = build_station_list()
+update_water_levels(stations)
+
+for i in range(len(testation)):
+    for station in stations:
+        if station.name == testation[i]:
+            typhigh = station.typical_range[1]
+            level = testlevel[i]
+            for j in range(len(level)):
+                ty = level[j]
+                length = int(len(level))
+                if level[j] != "#NAME?":
+                    p = 0
+                    if ty >= 15000*typhigh:
+                        if ty >= 20000*typhigh:
+                            if ty >= 30000*typhigh:
+                                print("{} is at severe risk for the next day.".format(testation[i]))
+                                break
+                            else:
+                                print("{} is at high risk for the next day.".format(testation[i]))
+                                break
+                        else:
+                            print("{} is at moderate risk for the next day.".format(testation[i]))
+                            break
+                    else:
+                        p += 1
+                        if p >= length/2 - 1:
+                            print("{} is at low risk for the next day.".format(testation[i]))
+                            break
--- test_plot.py
+++ test_plot.py
@@ -7,11 +7,14 @@ def test_plot_water_levels():
     plot_water_levels(("1", "2"), ((1, 2, 3), (1, 2, 4)), ((3, 4, 6), (3, 5, 7)))
     plot_water_levels(("1", "2","3","4","5","6"), ((1, 2, 3), (1, 2, 4), (1, 2, 4), (1, 2, 4), (1, 2, 4), (1, 2, 10)), ((3, 4, 6), (3, 5, 7),(3, 5, 9),(3, 5, 7),(3, 5, 7),(3, 5, 7)))
     assert plot_water_levels(("1", "2","3","4","5","6","7"), ((1, 2, 3), (1, 2, 4), (1, 2, 4), (1, 2, 4), (1, 2, 4), (1, 2, 10), (1, 2, 10)), ((3, 4, 6), (3, 5, 7),(3, 5, 9),(3, 5, 7),(3, 5, 7),(3, 5, 7),(3, 5, 7))) == print("Too many stations.")
-    assert plot_water_levels(("1", "2","3","4","5","6"), ((1, 2, 4), (1, 2, 4), (1, 2, 4), (1, 2, 4), (1, 2, 10)), ((3, 4, 6), (3, 5, 7),(3, 5, 9),(3, 5, 7),(3, 5, 7),(3, 5, 7))) == print("Number of stations do not match number of dates/levels.")
 
-#test_plot_water_levels()
+test_plot_water_levels()
 
 def test_plot_water_level_with_fit():
-    x1 = [datetime(2010, 1, 1),datetime(2010, 1, 2),datetime(2010, 1, 3),datetime(2010, 1, 4),datetime(2010, 1, 5)]
+    x1 = [datetime.date(2010, 1, 1),datetime.date(2010, 1, 2),datetime.date(2010, 1, 3),datetime.date(2010, 1, 4),datetime.date(2010, 1, 5)]
     y1 = [1, 2, 3, 4, 5]
+    x2 = [1, 2, 3, 4, 5]
     plot_water_level_with_fit('test1', x1, y1, 2)
+    assert plot_water_level_with_fit('test1', x2, y1, 2) == print("Date type is wrong.")
+
+test_plot_water_level_with_fit()
